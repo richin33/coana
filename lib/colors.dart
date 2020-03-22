b2976e4aa9bb4450a14add02f78159ec5ba29765
Malte Dölker <mdoelker@me.com>
2020-03-22 14:53:10 +0100

    Add color definition

---
+++ lib/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// This should all be moved to ThemeData
+Color bgColor = const Color.fromARGB(0xFF, 0x1C, 0x71, 0xF2);
+Color buttonColor = const Color.fromARGB(0xFF, 0x1C, 0x71, 0xF2);
+Color buttonTextColor = const Color.fromARGB(0xFF, 0xFF, 0xFF, 0xFF);
+Color textColor = const Color.fromARGB(0xFF, 0x12, 0x2A, 0x48);
+Color boxBgColor = const Color.fromARGB(0xFF, 0xFF, 0xFF, 0xFF);
+Color boxBorderColor = const Color.fromARGB(0x0A, 0x1B, 0x6E, 0x33);
+Color buttonBorderColor = const Color.fromARGB(0x06, 0x35, 0x6B, 0x40);
+
+TextStyle headlineTextStyle =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w800,
+  fontSize: 16,
+  color: textColor
+);
+
+TextStyle paragraphTextStyle = TextStyle(
+  fontFamily: 'Montserrat',
+  fontSize: 14,
+  color: textColor,
+);
