86e16f95875eeddec047cc54864fc58e297aaeec
Richard Nitsche <richardnitsche33@google.com>
2020-03-22 11:48:39 +0100

    startseite

--- lib/main.dart
+++ lib/main.dart
@@ -10,92 +10,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'coana - Corona Anamnese App',
       theme: ThemeData(
+        primaryColor: Color.fromARGB(0xFF, 0xEE, 0xF2, 0xF8),
           fontFamily: 'Montserrat'
       ),
       home: Home(),
     );
   }
 }
-
-
-Widget hackathonLogoSection = Container(
-  padding: EdgeInsets.all(16.0),
-  child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Image(
-          image: AssetImage('assets/Logo_Projekt_01.png'),
-          height: 130,
-        ),
-      ],
-  ),
-);
-
-Widget introTextSection = Container(
-  padding: EdgeInsets.all(16.0),
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Image(
-                image: AssetImage('assets/logos/256.png'),
-                height: 130,
-              ),
-            ),
-            ],
-          ),
-      )
-    ],
-  ),
-);
-
-Widget coanaSection = Container(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-
-      RaisedButton(
-        onPressed: () {},
-        textColor: Colors.white,
-        padding: const EdgeInsets.all(0.0),
-        child: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: <Color>[
-                Color(0xFF0D47A1),
-                Color(0xFF1976D2),
-                Color(0xFF42A5F5),
-              ],
-            ),
-          ),
-          padding: const EdgeInsets.all(10.0),
-          child: const Text(
-              "Hier geht's zum Check",
-              style: TextStyle(fontSize: 20)
-          ),
-        ),
-      ),
-    ],
-  ),
-);
-
-
-Widget appImagesSection = Container(
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      Image(
-        image: AssetImage('assets/google-play-badge.png'),
-        height: 30,
-      ),
-      Image(
-        image: AssetImage('assets/ios-app-store-badge.png'),
-        height: 30,
-      ),
-    ],
-  ),
-);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -29,6 +29,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     setState(() => _screenSize = screenWidth(context: context));
     return Scaffold(
+      backgroundColor: primaryColor,
       appBar: AppBar(
         leading: Image(image: AssetImage('assets/logos/152.png')),
           backgroundColor: primaryColor
@@ -43,6 +44,79 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+
+
+  Widget hackathonLogoSection = Container(
+    padding: EdgeInsets.all(16.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image(
+          image: AssetImage('assets/Logo_Projekt_01.png'),
+          height: 130,
+        ),
+      ],
+    ),
+  );
+
+
+
+
+  Widget introTextSection = Container(
+    padding: EdgeInsets.all(16.0),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [Padding(
+              padding: EdgeInsets.all(11.0),
+              child: Image(
+                image: AssetImage('assets/logos/256.png'),
+                height: 130,
+              ),
+            ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+
+  Widget coanaSection = Container(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        RaisedButton(
+          color: Color.fromARGB(0xFF, 0xEE, 0xF2, 0xF8),
+          onPressed: () {},
+          child: const Text(
+              "Hier geht's zum Check",
+              style: TextStyle(fontSize: 20)
+          ),
+        ),
+      ],
+    ),
+  );
+
+
+  Widget appImagesSection = Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image(
+          image: AssetImage('assets/google-play-badge.png'),
+          height: 30,
+        ),
+        Image(
+          image: AssetImage('assets/ios-app-store-badge.png'),
+          height: 30,
+        ),
+      ],
+    ),
+  );
+
 
   @override
   void dispose() {
